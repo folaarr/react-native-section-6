c5a9432aabc5d64d534fce4c32ca43078007323e
Folajimi Abolade <folajimiabolade@gmail.com>
2025-11-06 19:47:16 +0100

    render all the categoties on my interface, first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { CATEGORIES } from './data/dummy-data';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <StatusBar style="dark" />
+          <SafeAreaView style={{flex: 1}}>
+            {/* <View style={styles.category}></View> */}
+            {/* backgroundColor: "black", */}
+            {CATEGORIES.map((item, index) => (
+              <View key={index} style={[styles.category, {backgroundColor: item.color}]}>
+                <Text>{item.title}</Text>
+              </View>
+              ))}
+          </SafeAreaView>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  category: { 
+    width: 75,
+    height: 75,
+    borderRadius: 15,
+    marginBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
